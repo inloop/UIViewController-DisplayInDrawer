4c7bca4c8e121e99b2e90f10720fbb98a32f9877
Vilém Kurz <vilem.kurz@cocoaminers.com>
2018-07-12 16:38:44 +0200

    Review comments

--- UIViewController-DisplayInDrawer/Classes/OutsideBoundsHittableView.swift
+++ UIViewController-DisplayInDrawer/Classes/OutsideBoundsHittableView.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 //inspired by https://stackoverflow.com/questions/11770743/capturing-touches-on-a-subview-outside-the-frame-of-its-superview-using-hittest
-class OutsideBoundsHittableView: UIView {
+final class OutsideBoundsHittableView: UIView {
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard !clipsToBounds else { return super.hitTest(point, with: event) }
         for subview in subviews.reversed() {
